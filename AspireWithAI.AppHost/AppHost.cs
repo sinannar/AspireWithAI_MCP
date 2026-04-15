c8ef2c9591be4132ba95be90c3a43890f73c27e5
Sinan Nar <sinan.nar@gmail.com>
2026-04-16 01:39:08 +1200

    adding projects to apphost and running

--- AspireWithAI.AppHost/AppHost.cs
+++ AspireWithAI.AppHost/AppHost.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var mcp = builder.AddProject<Projects.AspireWithAI_Mcp>("mcp");
+
+builder.AddProject<Projects.AspireWithAI_Api>("api")
+    .WithReference(mcp).WaitFor(mcp);
+
 builder.Build().Run();
