fa48216677b2a38f261b4a77bb5e43113655c9dd
Sinan Nar <sinan.nar@gmail.com>
2026-04-16 02:11:51 +1200

    mcp server implemented

--- AspireWithAI.Mcp/Program.cs
+++ AspireWithAI.Mcp/Program.cs
@@ -1,41 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.ComponentModel;
+using ModelContextProtocol.Server;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+builder.Services.AddMcpServer()
+    .WithHttpTransport(options =>
+    {
+        options.Stateless = true;
+    })
+    .WithToolsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapDefaultEndpoints();
+app.MapMcp("/mcp");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+[McpServerToolType]
+public static class WeatherDescriberMcpTools
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    [McpServerTool, Description("Describes the weather for given degree in celcius")]
+    public static string DescribeWeather(
+        [Description("degree in celcius")] int degree) => degree switch
+        {
+            < 0 => "Freezing",
+            < 10 => "Cold",
+            < 20 => "Mild",
+            < 30 => "Warm",
+            _ => "Hot"
+        };
 }
